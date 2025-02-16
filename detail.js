dd7dea79c7adc3a74618af42fd589eb47302dd09
sergio <elmatadorsergio1234@gmail.com>
2025-02-16 15:27:56 -0400

    servicio de credimotos

---
+++ detail.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+sr.reveal('.p1 ', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.p2 ', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.p3 ', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.p4 ', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+});
+
+
+
